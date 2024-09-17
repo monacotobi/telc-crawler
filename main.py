05ab8860a76d0f32f5dfa5e04f9e5e1f71cd4754
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 10:25:58 +0200

    added fetch_page function

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ URL = 'https://www.sprachartberlin.de/de/telc-pruefung-ergebnis-telc-exam-result
 CHECK_STRING = 'NOCH NICHT DA / NOT YET AVAILABLE'
 ALERT_STRING = 'SCHON DA / AVAILABLE'
 
+def fetch_page(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Raises HTTPErrror for bad responses
+        return response.text
+    except requests.RequestException as e:
+        print(f'Error fetching the page: {e}')
+        return None
+    except Exception as e:
+        print(f'An Exception occured: {e}')
+        return None
