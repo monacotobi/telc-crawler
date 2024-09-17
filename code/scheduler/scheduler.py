5910dd54f6cec9931bc9fb4bf0563ee0ab02f3ef
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 14:34:18 +0200

    added logger

--- code/scheduler/scheduler.py
+++ code/scheduler/scheduler.py
@@ -3,12 +3,15 @@ import asyncio
 from code.crawler.crawler import crawl
 from code.telegram import bot
 from code.utils.config import USERS
+from code.utils.utils import logger
 
 async def scheduler():
+    logger.info('Starting scheduler')
     messages = crawl()
     results = ['🥳 20.07.2024: SCHON DA', '🥳 20.07.2024: B1 SCHON DA']
     for user_id in USERS:
         for message in messages:
+            logger.info(f'Sending message to {user_id}: {message}')
             if message in results:
                 await bot.send_message(user_id=user_id, text=message)
 
