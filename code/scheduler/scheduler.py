05f9a17481c3a36d65add351ac3bf10c93e02911
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 14:09:00 +0200

    added check if message is positive, only then send message

--- code/scheduler/scheduler.py
+++ code/scheduler/scheduler.py
@@ -6,9 +6,11 @@ from code.utils.config import USERS
 
 async def scheduler():
     messages = crawl()
+    results = ['🥳 14.09.2024: SCHON DA', '🥳 14.09.2024: B1 SCHON DA']
     for user_id in USERS:
         for message in messages:
-            await bot.send_message(user_id=user_id, text=message)
+            if message in results:
+                await bot.send_message(user_id=user_id, text=message)
 
 if __name__ == "__main__":
     asyncio.run(scheduler())
