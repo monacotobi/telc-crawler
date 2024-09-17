805bca77876fc17d6abd4bdc63d5f0881d98a2c6
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 12:38:45 +0200

    added scheduler function

--- code/scheduler/scheduler.py
+++ code/scheduler/scheduler.py
@@ -1,2 +1,14 @@
-def scheduler():
-    
+import asyncio
+
+from code.crawler.crawler import crawl
+from code.telegram import bot
+from code.utils.config import USERS
+
+async def scheduler():
+    messages = crawl()
+    for user_id in USERS:
+        for message in messages:
+            await bot.send_message(user_id=user_id, text=message)
+
+if __name__ == "__main__":
+    asyncio.run(scheduler())
