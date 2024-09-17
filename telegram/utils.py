7f478b5f23bf4db7c6906b8a978a5d14d7db3f32
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 11:25:19 +0200

    added extract webhook data function

--- telegram/utils.py
+++ telegram/utils.py
@@ -35,3 +35,20 @@ def validate_webhook_data(data:dict, required_keys:list) -> bool:
             return False
                 
     return True
+
+async def extract_webhook_data(data):
+    webhook_data = {
+        'id': None,
+        'message': None
+    }
+    
+    # Extracting user information
+    webhook_data['id'] = data['message']['from']['id']
+    
+    # Extracting message information
+    if 'text' in data['message']:
+        webhook_data['message'] = data['message']['text']
+    else:
+        logger.info(f"Message type not supported: {data['message']}")
+        raise Exception(f'Media type not supported: {data}')    
+    return webhook_data
