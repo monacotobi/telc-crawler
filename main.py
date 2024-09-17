8e3afa458aafacaac188a968f855904aad01615a
tobiasilgner <tobias.ilgner@gmail.com>
2024-09-17 10:24:11 +0200

    added config

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Configuration
+URL = 'https://www.sprachartberlin.de/de/telc-pruefung-ergebnis-telc-exam-result/'
+CHECK_STRING = 'NOCH NICHT DA / NOT YET AVAILABLE'
+ALERT_STRING = 'SCHON DA / AVAILABLE'
+
